1c5743615770701a7787704dce5de3b63b000481
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2024-07-29 01:12:00 +0530

    Added Request To Force Sub

--- plugins/commands.py
+++ plugins/commands.py
@@ -1131,6 +1131,19 @@ async def removetutorial(bot, message):
     await save_group_settings(grpid, 'is_tutorial', False)
     await reply.edit_text(f"<b>Successfully Removed Your Tutorial Link!!!</b>")
 
+@Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
+async def add_join_requests(client, message: ChatJoinRequest):
+  user = message.from_user.id
+  if not await db.find_join_req(user):
+    await db.add_join_req(user)
+
+@Client.on_message(filters.command("delete_request") & filters.user(ADMINS))
+async def delete_join_request(client, message):
+  try:
+     await db.del_join_req()    
+     await message.reply("**Deleted All Join Requests From DB 🙂**")
+  except:
+     logger.error("Can't Delete CJR", e)
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def stop_button(bot, message):
